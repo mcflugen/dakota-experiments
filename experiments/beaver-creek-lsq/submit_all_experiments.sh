1c7b6572b17132445f3e2da170cf4b3c6c05d4ca
mcflugen <huttone@colorado.edu>
2015-10-04 16:14:21 -0600

    Changed output directory name.

--- experiments/beaver-creek-lsq/submit_all_experiments.sh
+++ experiments/beaver-creek-lsq/submit_all_experiments.sh
@@ -2,8 +2,8 @@
 
 ALL_EXPERIMENTS="log_model_global peckham_model power_model_local log_model power_model_global log_model_local power_model"
 
-cd _experiments_neg_p
+cd _experiments_neg_cp
 for name in $ALL_EXPERIMENTS; do
   export EXPERIMENT_NAMES=$name
-  qsub ../run_experiments.sh -N "dakota-$name-neg_p" -v EXPERIMENT_NAMES -l nodes=1:ppn=1
+  qsub ../run_experiments.sh -N "dakota-$name-neg_cp" -v EXPERIMENT_NAMES -l nodes=1:ppn=1
 done
